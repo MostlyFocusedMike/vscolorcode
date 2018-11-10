75e1b1c0c5c7b63f6feba86db1ee730275a39604
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-11-10 01:33:44 -0500

    get it working with hard coded colors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ console.log(process.cwd())
 var fs = require('fs');
 var dir = './.vscode';
 var settingsFile = './.vscode/settings.json'
+const customs = "workbench.colorCustomizations"
 
 const colorOptions = {
     green: {
@@ -14,14 +15,25 @@ const colorOptions = {
             "statusBar.background": "#111",
             "statusBar.foreground": "#ccc"
         }
-    }
+    },
+    blue: {
+        "workbench.colorCustomizations": {
+            "titleBar.activeForeground": "#fff",
+            "titleBar.inactiveBackground": "#0000bb",
+            "titleBar.activeBackground": "#000099",
+            "activityBar.background": "#222244",
+            "statusBar.background": "#111",
+            "statusBar.foreground": "#ccc"
+        }
+    },
 
 }
 if (!fs.existsSync(dir)){
     fs.mkdirSync(dir);
     fs.writeFileSync("./.vscode/settings.json", JSON.stringify(colorOptions.green))
-} if (fs.existsSync(settingsFile)) {
-    console.log('It already exists')
-    const settings = require('./.vscode/settings.json')
-    console.log('settings:', settings)
+} else {
+    if (!fs.existsSync(settingsFile)) fs.writeFileSync("./.vscode/settings.json", JSON.stringify(colorOptions.green))
+    const settings = require('../.vscode/settings.json')
+    settings[customs] = {...settings[customs], ...colorOptions.green[customs]}
+    fs.writeFileSync("./.vscode/settings.json", JSON.stringify(settings))
 }
