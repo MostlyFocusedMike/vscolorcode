585c3be1b3b852e46f8a78dcef5bfbe399ad1ff9
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-11-11 23:40:16 -0500

    minor copy edits

--- src/index.js
+++ src/index.js
@@ -23,8 +23,8 @@ function alias() {
     console.log('alias vsblue="vscolor blue"')
     console.log('alias vspurple="vscolor purple"')
     console.log('alias vsblack="vscolor black"')
-    console.log('# alias [CUSTOM COLOR HERE]="vscolor -c [TITLE BAR HEX] [ACTIVITY BAR HEX] [FONT HEX]"\n')
-    console.log('alias vsgray="vscolor -c 999 222 000"    # do not use \'#\' with the hex codes')
+    console.log('# alias [CUSTOM COLOR HERE]="vscolor -c [TITLE BAR HEX] [ACTIVITY BAR HEX] [FONT HEX]"')
+    console.log('alias vsgray="vscolor -c 999 222 000"    # do not use \'#\' with the hex codes\n')
 }
 
 function setColorStd(color) {
