add5b70f3dc6405ab67bce51fbf7476198122f25
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-11-11 22:50:06 -0500

    add some more error handling

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const arg1 = process.argv[2]
 const colorOptions = {
     red: {
         "workbench.colorCustomizations": {
-            "titleBar.activeForeground": "#fff",
+            "titleBar.activeForeground": "#ddd",
             "titleBar.activeBackground": "#ab0000",
             "activityBar.background": "#5e0b0b",
             "statusBar.background": "#111",
@@ -77,28 +77,31 @@ const colorOptions = {
             "statusBar.foreground": "#ccc"
         }
     },
-    white: {
-        "workbench.colorCustomizations": {
-            "titleBar.activeForeground": "#000",
-            "titleBar.activeBackground": "#FFF",
-            "activityBar.background": "#DDD",
-            "activityBar.foreground": "#111",
-            "statusBar.background": "#111",
-            "statusBar.foreground": "#ccc"
-        }
-    },
+}
+
+function help() {
+    console.log("The standard options are:\nred, orange, yellow, green, blue, purple, or black.")
+    console.log("\nYou can also use the -c option for custom colors:\n\n\tvscolor -c [title bar hex] [activity bar hex] [font hex]\n")
+    console.log("Here is a lighter blue:\n\n\tvscolor -c 4dc3ff 0099e6 FFF\n")
+    console.log("Do not include the '#' when listing out the colors\n")
 }
 
 function setColorStd(color) {
+    if (!colorOptions[color]) {
+        console.log("\nLooks like that color isn't available.")
+        help()
+        return false;
+    }
     if (!fs.existsSync(vscodeDir)){
         fs.mkdirSync(vscodeDir);
-        fs.writeFileSync("./.vscode/settings.json", JSON.stringify(colorOptions[color]))
+        fs.writeFileSync("./.vscode/settings.json", JSON.stringify(colorOptions[color]));
     } else {
         if (!fs.existsSync(settingsJSON)) fs.writeFileSync("./.vscode/settings.json", JSON.stringify(colorOptions[color]));
-        const settings = JSON.parse(fs.readFileSync('./.vscode/settings.json', 'utf8')); // for some reason require() won't work, maybe becuase it's a hidden file? 
-        settings[workbenchStr] = {...settings[workbenchStr], ...colorOptions[color][workbenchStr]}
-        fs.writeFileSync("./.vscode/settings.json", JSON.stringify(settings))
+        const settings = JSON.parse(fs.readFileSync('./.vscode/settings.json', 'utf8')); 
+        settings[workbenchStr] = {...settings[workbenchStr], ...colorOptions[color][workbenchStr]};
+        fs.writeFileSync("./.vscode/settings.json", JSON.stringify(settings));
     }
+    return true;
 }
 
 function setColorCustom() {
@@ -107,10 +110,10 @@ function setColorCustom() {
     let font = "#FFF";
     
     if (process.argv.length === 5) { // title bar color, font color
-        font = `#${process.argv[4]}`
+        font = `#${process.argv[4]}`;
     } else if (process.argv.length === 6) { // title bar color, activity bar color, font color
-        activityBar = `#${process.argv[4]}`
-        font = `#${process.argv[5]}`
+        activityBar = `#${process.argv[4]}`;
+        font = `#${process.argv[5]}`;
     }
     colorOptions.custom = {
         "workbench.colorCustomizations": {
@@ -120,13 +123,15 @@ function setColorCustom() {
             "statusBar.background": "#111",
             "statusBar.foreground": "#ccc"
         }
-    }
-    setColorStd("custom")
+    };
+    setColorStd("custom");
 }
 
 function setColors() {
     if (arg1 === "-c") {
         setColorCustom()
+    } else if (arg1 === "-h" || arg1 === "--help") {
+        help() 
     } else {
         setColorStd(arg1)
     }
